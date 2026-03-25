2b2dde2da3e73f1695ced294a62448eb5d72dbce
AP Adhnan <apadhnan@gmail.com>
2026-03-25 19:16:17 +0530

    saved

--- credit_card/app.py
+++ credit_card/app.py
@@ -3,16 +3,16 @@ import streamlit as st
 import joblib
 import numpy as np
 
-st.title("Credit Fraud Detection")
+st.title("Credit Card Fraud Detection")
 
 model = joblib.load("C:\\Users\\apadh\\OneDrive\\Desktop\\project\\credit_card\\rf.pkl")
 scaler = joblib.load("C:\\Users\\apadh\\OneDrive\\Desktop\\project\\credit_card\\scaler.pkl")
 
 amount = st.number_input("Transaction Amount")
-transaction_hour = st.slider("Transaction Hour", 0, 23)
+transaction_hour = st.number_input("Transaction Hour")
 foreign_transaction = st.selectbox("Foreign Transaction", ["No", "Yes"])
 location_mismatch = st.selectbox("Location Mismatch", ["No", "Yes"])
-device_trust_score = st.slider("Device Trust Score", 0.0, 1.0)
+device_trust_score = st.number_input("Device Trust Score")
 velocity_last_24h = st.number_input("Transactions in last 24h")
 foreign_transaction = {'No': 0, 'Yes': 1}[foreign_transaction]
 location_mismatch = {'No': 0, 'Yes': 1}[location_mismatch]
